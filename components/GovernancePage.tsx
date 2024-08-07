32f6637679c2fa4e8769015633eef0c867549372
Paul <mierlo.paul@hotmail.nl>
2024-08-07 10:06:22 +0200

    included logic for abi

--- components/GovernancePage.tsx
+++ components/GovernancePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
@@ -8,17 +8,70 @@ import {
   CardTitle,
   CardContent,
   CardDescription,
-  CardFooter
+  CardFooter,
 } from "@/components/ui/card"
+import { useAccount, useWriteContract } from 'wagmi';
+import { IDKitWidget, ISuccessResult } from '@worldcoin/idkit';
+import { decodeAbiParameters, toHex } from 'viem';
+import WorldverifyAbi from "../abi/Worldverify.json"; // Adjust the path as necessary
+
+
 const GovernancePage = () => {
+
+  const { address: connectedAddress } = useAccount(); // Get type address from useAccount and assigning it to const connectedAddress
+
+
+  
+  const { writeContractAsync: writeYourContractAsync } = useWriteContract({
+    address: '0xYourContractAddressHere', // Replace with your contract address
+    abi: WorldverifyAbi/* ABI of your contract */, // Replace with your contract ABI
+    functionName: 'registerPatient',
+  });
+
+  const onSuccess = async (result: ISuccessResult) => {
+    const unpackedProof = decodeAbiParameters([{ type: 'uint256[8]' }], result.proof as 0x${string})[0];
+    console.log('result', result);
+    console.log('unpackedProof', unpackedProof);
+
+
+    try {
+    await writeYourContractAsync({
+      args: [
+        connectedAddress,
+        BigInt(result.merkle_root),
+        BigInt(result.nullifier_hash),
+        unpackedProof,
+        toHex('0xbafkreibpppzeta6odb3k25ctwwqqq3zxqa4v67k3lc7ryh7dw2vcjot63u'),
+      ],
+    });
+    console.log('Function called');} catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
+
   return (
     <div className="flex flex-col items-center mt-4">
-    <Card className="sm:col-span-2 p-4 min-w-7/12 w-9/12">
-      <CardHeader className="flex flex-row items-center justify-between">
-        <CardTitle className="text-3xl">Proposals</CardTitle>
-        <Button>{"+ New Proposal"}</Button>
-      </CardHeader>
-    </Card>
+      <Card className="sm:col-span-2 p-4 min-w-7/12 w-9/12">
+        <CardHeader className="flex flex-row items-center justify-between">
+    
+          <CardTitle className="text-3xl">
+                Proposals
+              </CardTitle>
+              <IDKitWidget
+            app_id="app_GBkZ1KlVUdFTjeMXKlVUdFT" // must be an app set to on-chain in Developer Portal
+            action="claim_nft"
+            signal={connectedAddress} // proof will only verify if the signal is unchanged, this prevents tampering
+            onSuccess={onSuccess} // use onSuccess to call your smart contract
+            // no use for handleVerify, so it is removed
+            // use default verification_level (orb-only), as device credentials are not supported on-chain
+          >
+            {({ open }) => (
+              <Button onClick={open}>{"+ New Proposal"}</Button>
+            )}
+          </IDKitWidget>
+        </CardHeader>
+      </Card>
       <div className="grid flex-1 mt-4 items-start gap-4 p-4 min-w-7/12 w-9/12 sm:px-6 sm:py-0 md:gap-8">
         <Tabs defaultValue="all">
           <div className="flex items-center">
@@ -35,22 +88,24 @@ const GovernancePage = () => {
             <div className="flex flex-col gap-4">
               <Card className="">
                 <CardHeader className="flex flex-row items-center justify-between">
-                    <p className="text-3xl font-semibold">Donation</p>
-                      <Badge className='text-lg' variant="outline">Active</Badge>
+                  <p className="text-3xl font-semibold">Donation</p>
+                  <Badge className="text-lg" variant="outline">
+                    Active
+                  </Badge>
                 </CardHeader>
                 <CardContent>
-                    <CardDescription>
-                        <p className="text-lg">Donation to the community</p>
-                    </CardDescription>
+                  <CardDescription>
+                    <p className="text-lg">Donation to the community</p>
+                  </CardDescription>
                 </CardContent>
                 <CardFooter className="text-sm">
-                Published by 0x1234...5678
+                  Published by 0x1234...5678
                 </CardFooter>
               </Card>
             </div>
           </TabsContent>
         </Tabs>
-    </div>
+      </div>
     </div>
   )
 }
