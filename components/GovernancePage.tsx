ca85600c5151a67c8321f003f412a07278440e91
Paul <mierlo.paul@hotmail.nl>
2024-08-07 12:23:49 +0200

    fixed abi contract write wagmi

--- components/GovernancePage.tsx
+++ components/GovernancePage.tsx
@@ -10,55 +10,56 @@ import {
   CardDescription,
   CardFooter,
 } from "@/components/ui/card"
-import { useAccount, useWriteContract } from 'wagmi';
-import { IDKitWidget, ISuccessResult } from '@worldcoin/idkit';
-import { decodeAbiParameters, toHex } from 'viem';
-import WorldverifyAbi from "../abi/Worldverify.json"; // Adjust the path as necessary
-
+import { useAccount, useWriteContract } from "wagmi"
+import { IDKitWidget, ISuccessResult } from "@worldcoin/idkit"
+import { decodeAbiParameters, toHex } from "viem"
+import WorldverifyAbi from "../abi/Worldverify.json" // Adjust the path as necessary
 
 const GovernancePage = () => {
-
-  const { address: connectedAddress } = useAccount(); // Get type address from useAccount and assigning it to const connectedAddress
-
-
-  
-  const { writeContractAsync: writeYourContractAsync } = useWriteContract({
-    address: '0xYourContractAddressHere', // Replace with your contract address
-    abi: WorldverifyAbi/* ABI of your contract */, // Replace with your contract ABI
-    functionName: 'registerPatient',
-  });
+  const { address: connectedAddress } = useAccount() // Get type address from useAccount and assigning it to const connectedAddress
 
   const onSuccess = async (result: ISuccessResult) => {
-    const unpackedProof = decodeAbiParameters([{ type: 'uint256[8]' }], result.proof as 0x${string})[0];
-    console.log('result', result);
-    console.log('unpackedProof', unpackedProof);
+    const {
+      data: hash,
+      isPending,
+      error,
+      writeContractAsync,
+    } = useWriteContract()
 
+    const unpackedProof = decodeAbiParameters(
+      [{ type: "uint256[8]" }],
+      result.proof as `0x${string}`
+    )[0]
+    console.log("result", result)
+    console.log("unpackedProof", unpackedProof)
 
     try {
-    await writeYourContractAsync({
-      args: [
-        connectedAddress,
-        BigInt(result.merkle_root),
-        BigInt(result.nullifier_hash),
-        unpackedProof,
-        toHex('0xbafkreibpppzeta6odb3k25ctwwqqq3zxqa4v67k3lc7ryh7dw2vcjot63u'),
-      ],
-    });
-    console.log('Function called');} catch (error) {
-      console.error('An error occurred:', error);
+      await writeContractAsync({
+        address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`, // Replace with your contract address
+        account: connectedAddress!,
+        abi: WorldverifyAbi, // Replace with your contract ABI
+        functionName: "registerAccount",
+        args: [
+          connectedAddress!,
+          BigInt(result!.merkle_root),
+          BigInt(result!.nullifier_hash),
+          unpackedProof,
+          toHex(
+            "0xbafkreibpppzeta6odb3k25ctwwqqq3zxqa4v67k3lc7ryh7dw2vcjot63u"
+          ),
+        ],
+      })
+    } catch (error) {
+      console.error("Error", error)
     }
-  };
-
+  }
 
   return (
     <div className="flex flex-col items-center mt-4">
       <Card className="sm:col-span-2 p-4 min-w-7/12 w-9/12">
         <CardHeader className="flex flex-row items-center justify-between">
-    
-          <CardTitle className="text-3xl">
-                Proposals
-              </CardTitle>
-              <IDKitWidget
+          <CardTitle className="text-3xl">Proposals</CardTitle>
+          <IDKitWidget
             app_id="app_GBkZ1KlVUdFTjeMXKlVUdFT" // must be an app set to on-chain in Developer Portal
             action="claim_nft"
             signal={connectedAddress} // proof will only verify if the signal is unchanged, this prevents tampering
@@ -66,9 +67,7 @@ const GovernancePage = () => {
             // no use for handleVerify, so it is removed
             // use default verification_level (orb-only), as device credentials are not supported on-chain
           >
-            {({ open }) => (
-              <Button onClick={open}>{"+ New Proposal"}</Button>
-            )}
+            {({ open }) => <Button onClick={open}>{"+ New Proposal"}</Button>}
           </IDKitWidget>
         </CardHeader>
       </Card>
